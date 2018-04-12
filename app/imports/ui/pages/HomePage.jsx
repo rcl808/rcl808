1d8b3be87e1d0983dba774b63cf56e18d3361aa7
Thinh Lam <thinh851@hawaii.edu>
2018-04-11 19:11:39 -1000

    added pictures to category

--- app/imports/ui/pages/HomePage.jsx
+++ app/imports/ui/pages/HomePage.jsx
@@ -13,38 +13,38 @@ class HomePage extends React.Component {
           <Grid stackable center container>
             <Grid.Row columns={4}>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Outdoors & Adventure</Header>
+                <Image rounded src='/images/academic.jpg' />
+                <Header as='h3'>Academic/Professional</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Tech</Header>
+                <Image rounded src='/images/sport.jpg' />
+                <Header as='h3'>Sports/Leisure</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Family</Header>
+                <Image rounded src='/images/religous.jpg' />
+                <Header as='h3'>Religious/Recreational</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Health & Wellness</Header>
+                <Image rounded src='/images/sorority.jpg' />
+                <Header as='h3'>Sorority/Fraternity</Header>
               </Grid.Column>
             </Grid.Row>
             <Grid.Row columns={4}>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Sports & Fitness</Header>
+                <Image rounded src='/images/culture.jpg' />
+                <Header as='h3'>Ethnic/Cultural</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Learning</Header>
+                <Image rounded src='/images/student.jpg' />
+                <Header as='h3'>Student Affairs</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Photography</Header>
+                <Image rounded src='/images/honor.jpg' />
+                <Header as='h3'>Honorary Society</Header>
               </Grid.Column>
               <Grid.Column textAlign='left'>
-                <Image rounded src='/images/placeholder.png' />
-                <Header as='h3'>Food & Drink</Header>
+                <Image rounded src='/images/election.JPG' />
+                <Header as='h3'>Election</Header>
               </Grid.Column>
             </Grid.Row>
           </Grid>
