6c5d164a826d458cd51d822cbcd4a7c879efdcdc
Thinh Lam <thinh851@hawaii.edu>
2018-04-11 22:59:47 -1000

    banner for Create Club page

--- app/imports/ui/pages/CreateClub.jsx
+++ app/imports/ui/pages/CreateClub.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Header, Container, List, Item } from 'semantic-ui-react';
+import { Header, Container, List, Item, Grid } from 'semantic-ui-react';
 
 class CreateClub extends React.Component {
   render() {
     return (
         <div>
+          <div className='create-club-page'>
+            <Grid divided='vertically' stackable center container>
+              <Grid.Row columns={1}>
+                <Grid.Column textAlign='center'>
+                  <Header as='h1' inverted>Start a new Club</Header>
+                  <Header as='h3' inverted>Time to #MakeManoaOurs</Header>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </div>
+          <div className='create-club-info'>
           <Container text>
             <Header as='h1' textAlign='left'>Forming an RIO (Registered Independent Organization)</Header>
             <Item>Forming an RIO requires at least 6 current UH Manoa students, faculty or staff members with a shared interest in establishing, developing, representing and being responsible for the proposed organization. All organizations must complete the folling registration steps:</Item>
@@ -15,6 +26,7 @@ class CreateClub extends React.Component {
             </List>
             <Item>New clubs are only added to Rad Club List from the list of registered RIO so please apply early in the semester.</Item>
           </Container>
+          </div>
         </div>
     );
   }
