963c64e53c2625f0a0d904aa0fc7dea13674e598
Reyn Mukai <reyn.mukai@gmail.com>
2018-04-17 09:51:28 -1000

    Added footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
+import { Container, Grid, Header, Icon } from 'semantic-ui-react';
+
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px', color: 'white' };
+    const divStyle = { paddingTop: '60px', color: 'black' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
+          <div className='footer-background'>
+            <Container textAlign='center'>
+              <Header as='h1' inverted>How Rad Club List Works</Header>
+              <br/>
+              <Grid container verticalAlign='middle' columns={4}>
+                <Grid.Column textAlign='right'>
+                  <Icon name='search' size='huge' color='blue' />
+                </Grid.Column>
+                <Grid.Column textAlign='left'>
+                  <Header as='h2' inverted>Find a Club</Header>
+                  Discover UH Clubs
+                  <br/>
+                  <a href="">
+                    Explore
+                    <Icon name='arrow right' color='blue' />
+                  </a>
+                </Grid.Column>
+                <Grid.Column textAlign='right'>
+                  <Icon name='add' size='huge' color='blue' />
+                </Grid.Column>
+                <Grid.Column textAlign='left'>
+                  <Header as='h2' inverted>Create a Club</Header>
+                  Create your own club to make UH Manoa the best place to be.
+                  <br/>
+                  <a href="">
+                    Create a Club
+                    <Icon name='right arrow' color='blue' />
+                  </a>
+                </Grid.Column>
+              </Grid>
+            </Container>
           </div>
         </footer>
     );
